e0e0c47bbb1659d99e9e4db101697a201047353e
MihaKavs <miha.kavs@gmail.com>
2024-02-06 17:38:28 +0100

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   src/comp/PostCreator.js

--- src/comp/PostCreator.js
+++ src/comp/PostCreator.js
@@ -1,23 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Editor.css'
+import axios from 'axios'
 function PostCreator() {
+    const [file, setFile] = useState();
+    const [uploadedFile, setUploadedFile] = useState();
+    const [error, setError] = useState();
+
+    function handleChange(event) {
+        setFile(event.target.files[0]);
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        const url = 'http://localhost:3000/uploadFile';
+        const formData = new FormData();
+        formData.append('file', file);
+        formData.append('fileName', file.name);
+        const config = {
+            headers: {
+                'content-type': 'multipart/form-data',
+            },
+        };
+        axios.post(url, formData, config)
+            .then((response) => {
+                console.log(response.data);
+                setUploadedFile(response.data.file);
+            })
+            .catch((error) => {
+                console.error("Error uploading file: ", error);
+                setError(error);
+            });
+    }
     return (
         <div className='PostCreator-container'>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className='PostCreator-title'>
                     <input type='text' className='title-input' placeholder='vnesi naslov'></input>
                 </div>
-                <div className='PostCreator-location'>
+                <div className='PostCreator-title'>
                     <input type='text' className='title-input' placeholder='vnesi lokacijo'></input>
                 </div>
                 <div className='PostCreator-description'>
                     <textarea className='description input' placeholder='opis'></textarea>
                 </div>
-                <div className='PostCreator-location'>
-                    <input type='file' multiple className='title-input' placeholder='vnesi slike'></input>
+                <div className='PostCreator-title'>
+                    <input type='file' multiple className='title-input' placeholder='vnesi slike' onChange={handleChange}></input>
                 </div>
-
             </form>
+            {uploadedFile && <img src={uploadedFile} alt="Uploaded content" />}
+            {error && <p>Error uploading file: {error.message}</p>}
         </div>
     )
 }
